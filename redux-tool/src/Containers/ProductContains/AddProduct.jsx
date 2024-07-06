c6ca9f3efb2fc9c0e1d6316c82a1114d4f5c8662
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-06 10:42:13 +0300

    Commit message >>>>> update the description input type <<<<<

--- redux-tool/src/Containers/ProductContains/AddProduct.jsx
+++ redux-tool/src/Containers/ProductContains/AddProduct.jsx
@@ -1,27 +1,26 @@
 import {Form} from "react-bootstrap";
-
+import "./../../Styling/AddProduct.css"
 
 const AddProduct = () => {
     return(<>
-            <Form >
+            <Form id={"product-input-form"}>
                 <Form.Group>
-                    <Form.Label htmlFor={"product-image"}>Product image.</Form.Label>
-                    <Form.Control id={"product-image"} type={"file"}  />
+                    <Form.Control id={"product-input-image"} type={"file"}  />
                 </Form.Group>
                 <div id={"product-text-input"}>
                   <Form.Group>
-                      <Form.Label htmlFor={"title"}>Product Title.</Form.Label>
+                      <Form.Label id={"title-label"} htmlFor={"title"}>Product Title.</Form.Label>
                       <Form.Control id={"title"} type={"text"} />
                   </Form.Group>
 
                     <Form.Group>
-                        <Form.Label htmlFor={"product-price"}>Product Price.</Form.Label>
+                        <Form.Label id={"product-label"} htmlFor={"product-price"}>Product Price.</Form.Label>
                         <Form.Control id={"product-price"} type={"text"} />
                     </Form.Group>
 
                     <Form.Group>
-                        <Form.Label htmlFor={"product-description"}>Product Description.</Form.Label>
-                        <Form.Control id={"product-description"} type={"text"} />
+                        <Form.Label id={"description-label"} htmlFor={"product-description"}>Product Description.</Form.Label>
+                        <Form.Control as="textarea" rows={3} id={"product-description"} />
                     </Form.Group>
                 </div>
 
